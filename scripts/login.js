f786debb4c42176f904191ac4fe33f045900bdac
Nicolas Leclerc <nl@spirotron.fr>
2011-02-19 11:57:49 +0100

    Reworked login.html.

--- scripts/login.js
+++ scripts/login.js
@@ -9,20 +9,5 @@ function handleLoginresult(data){
 	else if (!data.loggedIn)
 		setErrorMessage("Erreur de login.");
 	else
-		setLoggedIn(data.username);
-}
-
-function showLoginForm() {
-	$('#mainContent').html('');
-	$('#authenticationZone').append(
-		$('<form name="loginForm" id="loginForm" target="dummyFrame" action="dummy.txt" method="post">'+
-		'<input type="text" id="login" name="login" placeholder="identifiant">'+
-		'<input type="password" name="password" id="password" placeholder="mot de passe">'+
-		'<input type="submit" value="Valider">'+
-		'</form>').submit(handleLoginSubmit)
-	);
-	
-	if (!Modernizr.input.placeholder) {
-		// TODO: handle case without placeholder support.
-	}
+		jumpTo('.');
 }
