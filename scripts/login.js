e8f0a0d1cc7b9dc1f141b98955c8faa09b2e9c0e
Nicolas Leclerc <nl@spirotron.fr>
2011-02-20 00:34:44 +0100

    Added input placeholder workaround for older browsers.

--- scripts/login.js
+++ scripts/login.js
@@ -11,3 +11,28 @@ function handleLoginresult(data){
 	else
 		jumpTo('.');
 }
+
+function applyPlaceholderWorkaround(){
+	$('input').each(function(index, item){
+		item = $(item);
+		
+		var placeholder = item.attr('placeholder');
+		if (placeholder) {
+			item.addClass('placeholderWorkaround');
+			item.val(placeholder);
+			item.focus(function(){
+				if (item.val() == placeholder){
+					item.val('');
+					item.removeClass('placeholderWorkaround');
+				}
+			});
+			
+			item.blur(function(){
+				if (!item.val()){
+					item.addClass('placeholderWorkaround');
+					item.val(placeholder);
+				}
+			})
+		}
+	});
+}
