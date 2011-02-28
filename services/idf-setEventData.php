408c2e870bebbd3c0cd09947a9915ca5923a6de8
Nicolas Leclerc <nl@spirotron.fr>
2011-02-28 23:17:28 +0100

    Added some input data filtering.

--- services/idf-setEventData.php
+++ services/idf-setEventData.php
@@ -15,17 +15,25 @@ try {
 	else {
 		$dbc = new IdfCalendarDbConnector();
 		
+		foreach ($eventData as $key => $value)
+			if (is_string($value))
+				$eventData[$key] = trim($value);
+		
 		if (!isset($eventData['start_date']) || !preg_match('/^\d{4}-\d{2}-\d{2}$/', $eventData['start_date']))
 			throw new Exception('Date manquante ou invalide.');
 		
 		if (!isset($eventData['title']) || !$eventData['title'])
 			throw new Exception('Titre manquant.');
+		else
+			$eventData['title'] = preg_replace('/\s+/', ' ', $eventData['title']); // normalize title spaces.
 			
 		if (!isset($eventData['description']) || !$eventData['description'])
 			throw new Exception('Description manquante.');
-			
+		
 		if (!isset($eventData['max_participants']) || !$eventData['max_participants'])
 			$eventData['max_participants'] = 0;
+		else
+			$eventData['max_participants'] = max(intval($eventData['max_participants']), 0); // filter non int and negative values.
 			
 		$dateTokens = explode('-', $eventData['start_date']);
 		$eventData['day'] = $dateTokens[2];
--- services/setEventData.php
+++ services/setEventData.php
@@ -15,23 +15,31 @@ try {
 	else {
 		$dbc = new CalendarDbConnector();
 		
+		foreach ($eventData as $key => $value)
+			if (is_string($value))
+				$eventData[$key] = trim($value);
+		
 		if (!isset($eventData['start_date']) || !preg_match('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/', $eventData['start_date']))
 			throw new Exception('Date ou heure manquante ou invalide.');
 		
 		if (!isset($eventData['region_id']) || !$eventData['region_id'])
 			throw new Exception('Région manquante.');
-					
+		
 		if (!isset($eventData['title']) || !$eventData['title'])
 			throw new Exception('Titre manquant.');
-			
+		else
+			$eventData['title'] = preg_replace('/\s+/', ' ', $eventData['title']); // normalize title spaces.
+		
 		if (!isset($eventData['description']) || !$eventData['description'])
 			throw new Exception('Description manquante.');
-			
+		
 		if (!isset($eventData['location']) || !$eventData['description'])
 			throw new Exception('Lieu manquant.');
-			
+		
 		if (!isset($eventData['max_participants']) || !$eventData['max_participants'])
 			$eventData['max_participants'] = 0;
+		else
+			$eventData['max_participants'] = max(intval($eventData['max_participants']), 0); // filter non int and negative values.
 		
 		$dbc->setEventData($currentUser['id'], $eventData);
 	}
